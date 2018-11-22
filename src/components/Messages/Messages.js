ae1a704ab3701f414cf80c1d4c12d2c69047f533
Rhonda <rtinkham0408@gmail.com>
2018-11-22 13:19:25 -0500

    add MetaPanel component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Segment, Comment } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { setUserPosts } from '../../actions';
 import firebase from '../../firebase';
 import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
@@ -46,6 +48,7 @@ class Messages extends Component {
         messagesLoading: false
       });
       this.countUniqueUsers(loadedMessages);
+      this.countUserPosts(loadedMessages);
     });
   };
 
@@ -134,6 +137,21 @@ class Messages extends Component {
     this.setState({ numUniqueUsers });
   };
 
+  countUserPosts = messages => {
+    let userPosts = messages.reduce((acc, message) => {
+      if (message.user.name in acc) {
+        acc[message.user.name].count += 1;
+      } else {
+        acc[message.user.name] = {
+          avatar: message.user.avatar,
+          count: 1
+        }
+      }
+      return acc;
+    }, {});
+    this.props.setUserPosts(userPosts);
+  }
+
   displayMessages = messages => 
     messages.length > 0 && 
     messages.map(message => (
@@ -200,4 +218,7 @@ class Messages extends Component {
   }
 }
 
-export default Messages;
+export default connect(
+  null, 
+  { setUserPosts }
+)(Messages);
